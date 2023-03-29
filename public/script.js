5b87f69203cfcd359e334918a0aea0b19d288703
peppertaco <123547699+peppertaco@users.noreply.github.com>
2023-03-29 17:39:37 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -828,9 +828,9 @@ function addOneMessage(mes, type = "normal", insertAfter = null) {
             avatarImg = system_avatar;
         } else {
             if (characters[this_chid].avatar != "none") {
-                avatarImg = `/thumbnail?type=avatar&file=${encodeURIComponent(characters[this_chid].avatar)}`;
+                avatarImg = "characters/" + characters[this_chid].avatar;
                 if (is_mes_reload_avatar !== false) {
-                    avatarImg += "&" + is_mes_reload_avatar;
+                    avatarImg += "#" + is_mes_reload_avatar;
                 }
             } else {
                 avatarImg = default_avatar;
@@ -2598,9 +2598,9 @@ function select_selected_character(chid) {
     //$("#avatar_div").css("display", "none");
     var this_avatar = default_avatar;
     if (characters[chid].avatar != "none") {
-        this_avatar = "/thumbnail?type=avatar&file=" + encodeURIComponent(characters[chid].avatar);
+        this_avatar = "characters/" + characters[chid].avatar;
     }
-    $("#avatar_load_preview").attr("src", this_avatar + "&" + Date.now());
+    $("#avatar_load_preview").attr("src", this_avatar + "#" + Date.now());
     $("#name_div").css("display", "none");
 
     $("#form_create").attr("actiontype", "editcharacter");
